8ddea58035892904d05f352e8bf3b06482a5cad4
vaishnavi770 <vaishnavigavali2810@mail.com>
2024-09-04 23:23:45 +0530

    day7

---
+++ day7/p2.java
@@ -0,0 +1,14 @@
+class Demo{
+      public static void main(String[]args){
+	for(int i=1;i<=4;i++){
+	   for(int j=1;j<=4;j++){
+		if(j<=4-i){
+		   System.out.print("   ");
+		}else{
+		  System.out.print("  *");
+          }
+        }
+	 System.out.println();
+      }
+   }
+}
---
+++ day7/p4.java
@@ -0,0 +1,12 @@
+class Demo{
+	public static void main(String[]args){
+		for(int i=1;i<=4;i++){
+		   for(int j=0;j<=4-i;j++){
+			System.out.print("* ");
+		   }
+			System.out.println();
+        }
+    }
+}
+		   
+	
