248c2e105c0a919b7107c76bf88609fb68514795
vaishnavi770 <vaishnavigavali2810@mail.com>
2024-09-05 23:40:53 +0530

    day8

---
+++ day8/p3.java
@@ -0,0 +1,12 @@
+class Demo{
+	public static void main(String[]args){
+		for(int i=1;i<=4;i++){
+		  for(int j=1;j<=i;j++){
+			System.out.print("01 ");
+
+			}
+			System.out.println();
+		}
+       }
+}
+
---
+++ day8/p4.java
@@ -0,0 +1,15 @@
+class Demo{
+	public static void main(String[]args){
+		for(int i=1;i<=4;i++){
+		for(int j=1;j<=i;j++){
+		    if(j==2){
+			System.out.print("= ");
+		    }else{
+			System.out.print("+ ");
+		    }
+ 		}
+		System.out.println();
+	    }
+        }
+    }
+
